807992ed02a3b5cdb97b73e85880b1ab7b565092
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-01-15 10:18:28 +0000

    Created About page

---
+++ src/Components/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <div>
+        <p>
+          Since I started learning Front-End Development, I took the interest in
+          programming in ReactJs.
+        </p>
+        <p>
+          Despite the short experience in developing App's in ReactJs, i enjoy
+          programming in this framework. The interest in the language came from
+          a bad experience in a student intern. The language used was C# and
+          jQuery, but my interest wasn't there.
+        </p>
+        <p>
+          After the end of the student intern, I developed my first program in
+          ReactJs, making a simples ToDo List. When i finish this app, the
+          interest in programming in ReactJs grown and I decide to make my
+          career in the framework.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
