d80ce87ef2da903fb9a13623640cb31929a84e20
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-02-10 13:49:49 +0000

    Modified to a clean and shorter About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,26 +1,20 @@
 import React from "react";
+import "./Style/About.css";
+import Highlight from "./Highlight";
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <div>
-        <p>
-          Since I started learning Front-End Development, I took the interest in
-          programming in ReactJs.
-        </p>
-        <p>
-          Despite the short experience in developing App's in ReactJs, i enjoy
-          programming in this framework. The interest in the language came from
-          a bad experience in a student intern. The language used was C# and
-          jQuery, but my interest wasn't there.
-        </p>
-        <p>
-          After the end of the student intern, I developed my first program in
-          ReactJs, making a simples ToDo List. When i finish this app, the
-          interest in programming in ReactJs grown and I decide to make my
-          career in the framework.
-        </p>
+    <div className="about">
+      <div className="title">About</div>
+      <div className="text">
+        <div className="textDiv">
+          Since I started learning <Highlight text="Frontend Development" />, I
+          took interest in{" "}
+          <a href="https://reactjs.org">
+            <Highlight text="React" />
+          </a>
+          .
+        </div>
       </div>
     </div>
   );
