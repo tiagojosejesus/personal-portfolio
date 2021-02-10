6960b2cbbcd377a9affea8bb48471be2ff3415ca
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-02-10 13:40:36 +0000

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import { MdComputer } from "react-icons/md";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div className="NavBar">
+          <div key="Home" className="NavBar-link_Home">
+            <Link to="/">
+              <MdComputer />
+            </Link>
+          </div>
+          <div key="Projects" className="NavBar-link">
+            <Link to="/Projects">Projects</Link>
+          </div>
+          <div key="About" className="NavBar-link">
+            <Link to="/About">About</Link>
+          </div>
+          <div key="Contact" className="NavBar-link">
+            <Link to="/Contacts">Contacts</Link>
+          </div>
+        </div>
+        <Switch>
+          <Route path="/Projects" component={Projects} />
+          <Route path="/About" component={About} />
+          <Route path="/Contacts" component={Contact} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }
