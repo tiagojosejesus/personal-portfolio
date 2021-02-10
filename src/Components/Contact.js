3f8124393817c120e11467e1a2fa72501b24f164
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-02-10 13:48:44 +0000

    Modified to contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { BiMailSend, BiPhoneCall } from "react-icons/bi";
-import { FaLinkedin } from "react-icons/fa";
+import "./Style/Contact.css";
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <div>
-        <BiMailSend />
-        <h4>tiagojosepjesus@gmail.com</h4>
-      </div>
-      <div>
-        <FaLinkedin />
-        <h4>
-          <a href="https://www.linkedin.com/in/tiagojosejesus/">
-            Tiago José Jesus
-          </a>
-        </h4>
-      </div>
-      <div>
-        <BiPhoneCall />
-        <h4>(+351)914491938</h4>
+    <div className="contact">
+      <div className="title">Contact</div>
+      <div className="contactForm">
+        <form className="form">
+          <label name="firstName" className="contactLabel">
+            First Name
+          </label>
+          <input type="text" className="contactInput" />
+          <label name="lastName" className="contactLabel">
+            Last Name
+          </label>
+          <input type="text" className="contactInput" />
+          <label className="contactLabel">Message</label>
+          <textarea className="contactTextarea" placeholder="Message" />
+          <button type="submit" className="contactSubmit">
+            Send
+          </button>
+        </form>
       </div>
     </div>
   );
