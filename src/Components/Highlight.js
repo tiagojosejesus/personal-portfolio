8c426e5202a8aad0f6232a52f0f9245e1e621dee
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-02-10 13:36:48 +0000

    Created Highlights page

---
+++ src/Components/Highlight.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Style/Highlight.css";
+
+function Highlight({ text }) {
+  return <span className="highlightHome">{text}</span>;
+}
+
+export default Highlight;
