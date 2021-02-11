08bd150b02c241db38fb9a5e8bfb8d6c60ae08f8
Tiago Jose Jesus <tiagojosepjesus@gmail.com>
2021-02-11 15:31:30 +0000

    Commented future features

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import "./App.css";
 import React from "react";
 import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
-import { MdComputer } from "react-icons/md";
+// import { MdComputer } from "react-icons/md";
 import Home from "./Components/Home";
-import About from "./Components/About";
+// import About from "./Components/About";
 import Projects from "./Components/Projects";
 // import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Router>
         <div className="NavBar">
-          <div key="Home" className="NavBar-link_Home">
-            <Link to="/">
-              <MdComputer />
-            </Link>
+          <div key="Home" className="NavBar-link">
+            <Link to="/">Home</Link>
           </div>
           <div key="Projects" className="NavBar-link">
             <Link to="/Projects">Projects</Link>
           </div>
-          <div key="About" className="NavBar-link">
+          {/* <div key="About" className="NavBar-link">
             <Link to="/About">About</Link>
-          </div>
+          </div> */}
           {/* <div key="Contact" className="NavBar-link">
             <Link to="/Contacts">Contacts</Link>
           </div> */}
         </div>
         <Switch>
-          <Route path="/Projects" component={Projects} />
-          <Route path="/About" component={About} />
+          <Route path="/Projects">
+            <Projects />
+          </Route>
+          {/* <Route path="/About">
+            <About />
+          </Route> */}
           {/* <Route path="/Contacts" component={Contact} /> */}
-          <Route path="/" component={Home} />
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
       </Router>
       <Footer />
